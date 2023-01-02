b2202b6aca73ad9b8b86a7950921c861a6ece630
sergeybayandin <serezha-bayandin@mail.ru>
2023-01-02 22:13:24 +0700

    Did cli socket unnamed

--- src/cli/cli_socket.c
+++ src/cli/cli_socket.c
@@ -1,29 +1,10 @@
-#include "config.h"
-
 #include "cli/cli_socket.h"
 
-#include <sys/un.h>
 #include <sys/socket.h>
 
 int cli_socket(void)
 {
-	struct sockaddr_un addr;
-	socklen_t          addrlen = sizeof(addr);
-
-	int fd;
-
-	if ((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1)
-		return -1;
-
-	addr.sun_family = AF_UNIX;
-	strcpy(addr.sun_path, DAEMON_SOCKETNAME);
-
-	if (connect(fd, (struct sockaddr*)&addr, addrlen) == -1) {
-		cli_close(fd);
-		return -1;
-	}
-
-	return fd;
+	return socket(AF_UNIX, SOCK_DGRAM, 0);
 }
 
 int cli_close(int fd)
